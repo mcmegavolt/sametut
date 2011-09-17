9c6296d0c5dbb4044070039f8f6c87e30d1d26d5
Ivan <johnny.da.kid@gmail.com>
2011-09-17 21:44:02 +0300

    fixed associations saving

--- Gemfile
+++ Gemfile
@@ -7,7 +7,6 @@ gem 'rails', '3.1.0'
 
 gem 'sqlite3'
 
-
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
@@ -32,22 +31,10 @@ group :test do
   gem 'turn', :require => false
 end
 
-
-
 gem 'haml'
 gem 'haml-rails'
 
-
-
-
 gem 'json'
-
 gem 'rmagick'
-
 gem "paperclip", "~> 2.3"
 
-group :assets do
-  gem 'sass-rails', "~> 3.1.0"
-  gem 'coffee-rails', "~> 3.1.0"
-  gem 'uglifier'
-end
--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -3,9 +3,13 @@ class Applicant < ActiveRecord::Base
   has_one :profile, :dependent => :destroy
   accepts_nested_attributes_for :profile
   has_many :social_activities, :dependent => :destroy
-  accepts_nested_attributes_for :social_activities, :reject_if => lambda { |a| a[:social_activities].blank? }, :allow_destroy => true
+  accepts_nested_attributes_for :social_activities, :reject_if => :blank_fileds, :allow_destroy => true
 
   validates_presence_of :first_name, :middle_name, :last_name
   validates_associated :profile
+
+  def blank_fileds entry
+    entry[:activity_type].blank? && entry[:position].blank?
+  end
 
 end
