e07c5f03b466fe43105d1da171bca7bdfa014c30
mcmegavolt <mcmegavolt@gmail.com>
2011-09-26 21:48:32 +0300

    * js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,7 +19,7 @@ function remove_fields  (link) {
 
 function add_fields(link, association, content) {
   var new_id = new Date().getTime();
-  var regexp = new RegExp("new_" + association, "g")
+  var regexp = new RegExp("new_" + association, "g");
   $(link).up().insert({
     before: content.replace(regexp, new_id)
   });
