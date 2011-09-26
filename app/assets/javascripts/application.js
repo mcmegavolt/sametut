9da79ea4d6d76008424558b533cfad4a2b041022
Ivan <johnny.da.kid@gmail.com>
2011-09-26 15:59:55 +0300

    add activities

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,18 +9,13 @@
 //= require_tree .
 
 
-function remove_fields  (link) {
-
+function remove_fields(link) {
     $(link).prev("input[type=hidden]").val("1");
-    $(link).closest(".group_fields").hide();
-
+    $(link).closest(".fields").hide();
 }
 
-
 function add_fields(link, association, content) {
-  var new_id = new Date().getTime();
-  var regexp = new RegExp("new_" + association, "g")
-  $(link).up().insert({
-    before: content.replace(regexp, new_id)
-  });
+    var new_id = new Date().getTime();
+    var regexp = new RegExp("new_" + association, "g");
+    $(link).parent().before(content.replace(regexp, new_id));
 }
