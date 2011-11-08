1776e814219491f66d465082cdabd22efb632a3a
mcmegavolt <mcmegavolt@gmail.com>
2011-11-08 16:55:34 +0200

    Merge branch 'master' of github.com:mcmegavolt/sametut
    Conflicts:
    	app/assets/javascripts/application.js
    	app/views/users/_form.haml
    	app/views/users/_social_activity_fields.haml

--- app/models/education_degree_level.rb
+++ app/models/education_degree_level.rb
@@ -1,6 +1,4 @@
 class EducationDegreeLevel < ActiveRecord::Base
-  DEGREE_LEVELS = [['One', 1],['Two', 2],['Three', 3]].freeze
-  PROMPT_TEXT = ['-Select']
 
   has_many :educations
 
--- app/models/education_school_type.rb
+++ app/models/education_school_type.rb
@@ -1,6 +1,4 @@
 class EducationSchoolType < ActiveRecord::Base
-  SCHOOL_TYPES = [['One', 1],['Two', 2],['Three', 3]].freeze
-  PROMPT_TEXT = ['-Select'].freeze
 
   has_many :educations
 
