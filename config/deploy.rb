3902846cd88d475349e4aae4bac9749e293dc40b
Alex Ponomarenko <mcmegavolt@gmail.com>
2012-03-13 19:29:31 +0200

    12345

--- config/deploy.rb
+++ config/deploy.rb
@@ -56,36 +56,52 @@ end
 # Uploads
 # ==============================
 
-namespace :uploads do
+#namespace :uploads do
+#
+#  desc <<-EOD
+#    Creates the upload folders unless they exist
+#    and sets the proper upload permissions.
+#  EOD
+#  task :setup, :except => { :no_release => true } do
+#    dirs = uploads_dirs.map { |d| File.join(shared_path, d) }
+#    run "#{try_sudo} mkdir -p #{dirs.join(' ')} && #{try_sudo} chmod g+w #{dirs.join(' ')}"
+#  end
+#
+#  desc <<-EOD
+#    [internal] Creates the symlink to uploads shared folder
+#    for the most recently deployed version.
+#  EOD
+#  task :symlink, :except => { :no_release => true } do
+#    sudo "rm -rf #{release_path}/public/uploads"
+#    sudo "ln -nfs #{shared_path}/uploads #{release_path}/public/uploads"
+#  end
+#
+#  desc <<-EOD
+#    [internal] Computes uploads directory paths
+#    and registers them in Capistrano environment.
+#  EOD
+#  task :register_dirs do
+#    set :uploads_dirs,    %w(uploads uploads/applicant)
+#    set :shared_children, fetch(:shared_children) + fetch(:uploads_dirs)
+#  end
+#
+#  after       "deploy:finalize_update", "uploads:symlink"
+#  on :start,  "uploads:register_dirs"
+#
+#end
 
-  desc <<-EOD
-    Creates the upload folders unless they exist
-    and sets the proper upload permissions.
-  EOD
-  task :setup, :except => { :no_release => true } do
-    dirs = uploads_dirs.map { |d| File.join(shared_path, d) }
-    run "#{try_sudo} mkdir -p #{dirs.join(' ')} && #{try_sudo} chmod g+w #{dirs.join(' ')}"
+namespace(:customs) do
+
+  task :symlink, :roles => :app do
+    run <<-CMD
+      ln -nfs #{shared_path}/system/uploads #{release_path}/public/uploads
+    CMD
   end
 
-  desc <<-EOD
-    [internal] Creates the symlink to uploads shared folder
-    for the most recently deployed version.
-  EOD
-  task :symlink, :except => { :no_release => true } do
-    sudo "rm -rf #{release_path}/public/uploads"
-    sudo "ln -nfs #{shared_path}/uploads #{release_path}/public/uploads"
-  end
-
-  desc <<-EOD
-    [internal] Computes uploads directory paths
-    and registers them in Capistrano environment.
-  EOD
-  task :register_dirs do
-    set :uploads_dirs,    %w(uploads uploads/applicant)
-    set :shared_children, fetch(:shared_children) + fetch(:uploads_dirs)
-  end
-
-  after       "deploy:finalize_update", "uploads:symlink"
-  on :start,  "uploads:register_dirs"
+  after "deploy:symlink","customs:symlink"
+  after "deploy", "deploy:cleanup"
 
 end
+
+
+
