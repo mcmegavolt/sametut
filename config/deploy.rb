11a5f0ef326be07cae1c5c037df446d50131f38e
Alex Ponomarenko <mcmegavolt@gmail.com>
2012-03-13 16:17:36 +0200

    12345

--- config/deploy.rb
+++ config/deploy.rb
@@ -72,8 +72,8 @@ namespace :uploads do
     for the most recently deployed version.
   EOD
   task :symlink, :except => { :no_release => true } do
-    run "rm -rf #{release_path}/public/uploads"
-    run "ln -nfs #{shared_path}/uploads #{release_path}/public/uploads"
+    run "#{try_sudo} rm -rf #{release_path}/public/uploads"
+    run "#{try_sudo} ln -nfs #{shared_path}/uploads #{release_path}/public/uploads"
   end
 
   desc <<-EOD
