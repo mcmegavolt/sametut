164349d8db9c3790370891afae6a93169779c9fe
Alex Ponomarenko <mcmegavolt@gmail.com>
2012-03-13 22:00:01 +0200

    * ...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,8 +3,6 @@ SameTut::Application.configure do
   Encoding.default_external = "UTF-8"
 
 
-
-
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   :address => 'mail.neasmo.org.ua',
@@ -14,7 +12,7 @@ SameTut::Application.configure do
   :password => 'cCRuAS%Ls1g+',
   :authentication => :plain,
   :openssl_verify_mode  => 'none'
-}
+  }
 
 
   # Settings specified here will take precedence over those in config/application.rb
@@ -33,7 +31,7 @@ SameTut::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
