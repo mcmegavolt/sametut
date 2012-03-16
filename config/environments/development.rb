6a5c89df01f14a6d1ec744cdbbc06ea1baafdf60
Alex Ponomarenko <mcmegavolt@gmail.com>
2012-03-16 12:17:01 +0200

    * ...

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,7 @@ SameTut::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => '0.0.0.0:3000' }
 
   config.action_mailer.delivery_method = :letter_opener
 
