c2d7cd89c77bedb29217f3d1321bfb88fd6aec0b
Aleksansr Ponomarenko <mcmegavolt@gmail.com>
2013-07-31 01:05:23 +0300

    bundle update

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -48,7 +48,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
-    "/assets/image_" + [version_name, "default.png"].compact.join('_')
+    "" + [version_name, "default.png"].compact.join('_')
   end
 
   # Process files as they are uploaded:
