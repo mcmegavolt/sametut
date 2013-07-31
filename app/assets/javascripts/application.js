1d3521c4be2855dfee929744c4ac597555f713c4
Aleksansr Ponomarenko <mcmegavolt@gmail.com>
2013-08-01 01:41:37 +0300

    modified:   app/assets/javascripts/application.js 	renamed:    vendor/assets/javascripts/jquery.geocomplete.min.js -> app/assets/javascripts/jquery.geocomplete.min.js 	deleted:    vendor/assets/javascripts/ckeditor/config.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,17 @@
-// This is a manifest file that'll be compiled into including all the files listed below.
-// Add new JavaScript/Coffee code in separate files in this directory and they'll automatically
-// be included in the compiled file accessible from http://example.com/assets/application.js
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
-
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
 //= require jquery.geocomplete.min
+//= require jquery.ui.datepicker
 //= require bootstrap
+//= require boot_strap
+//= require plugins/bootstrap-image-gallery.min
+//= require plugins/jcaption
+//= require plugins/lightbox
+//= require ckeditor/config
+//= require gmaps4rails/gmaps4rails.base
+//= require gmaps4rails/gmaps4rails.googlemaps
 //= require rails.validations
-//= require_tree .
 
 $(document).ready(function(){
 
